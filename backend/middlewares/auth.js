7ab33538a2db891662e928d3925a7680da80b3cd
Artem Dubravtsev <a.dubravtsev@yandex.ru>
2023-11-07 18:48:28 +0200

    ПР15 тест без сек.ключа после линтера

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
